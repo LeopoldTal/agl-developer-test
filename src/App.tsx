67d5e0c7466c89c16b014b276789c1093d05db9c
Leopold Tal G <leopold.tal.dg@gmail.com>
2021-04-22 13:24:05 +0200

    fetch pets on app start

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import PetGroups from './components/PetGroups';
+import { useSortedCats } from './hooks/useSortedCats';
 import './App.css';
 
-const App: React.FunctionComponent = () => (
-	<div className="agl-app">
-		<p>TODO: app goes here</p>
-	</div>
-);
+const App: React.FunctionComponent = () => {
+	const sortedCats = useSortedCats();
+
+	return (
+		<div className="agl-app">
+			<PetGroups groups={sortedCats} />
+		</div>
+	);
+};
 
 export default App;
